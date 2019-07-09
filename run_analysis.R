549880d84bf8b2e497a09e0b4189bb172800a064
smatismi <47929013+smatismi@users.noreply.github.com>
2019-07-09 15:51:32 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
 # this is the programminig assignment 1 for the getting and cleaning data course
-# get data 
-df1= read.txt(
+# get data from a file
+ dataframe1 <- read.table(file_path, header = TRUE)
+    View(data)
